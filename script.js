27cbae8918c0240991a1228a293dc02b569e9516
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-07-11 15:31:03 +0100

    added fetch function

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const searchBtn = document.getElementById("search-button");
 const spriteContainer = document.getElementById("sprite-container");
 const pokemonList = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
+const fetchData = async () => {
+    try{
+        const res = await fetch(pokemonList);
+        const data = await res.json();
+        //call main function
+    }catch (err){
+        console.log(err);
+    }
+}
 
 
 
@@ -11,11 +20,10 @@ const pokemonList = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
 
 
-
-searchBtn.addEventListener('click',  ); 
+searchBtn.addEventListener('click', fetchData); 
 
 input.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
-    ; 
+    fetchData(); 
   }
 });

57a84bcccbb4de65fa37c476a9612f3d4a6224c4
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-07-11 15:25:03 +0100

    added event listeners, initialized html elements

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const spriteContainer = document.getElementById("sprite-container");
+const pokemonList = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
+
+
+
+
+
+
+
+
+
+
+searchBtn.addEventListener('click',  ); 
+
+input.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    ; 
+  }
+});
